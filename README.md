73b338534efbe0fb20701aa87e4569997f624c1f
Wodjess <aleshkatokareff@yandex.ru>
2022-01-12 22:11:32 +0300

    MainFiles

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BetterQuest
- 
+____
+# :hot_face::scream: BetterQuest типо классно:scream::hot_face:
+____
---
+++ betterquest/betterquest/PreLoad.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Windows;
+using System.Windows.Controls;
+namespace betterquest
+{
+    public partial class PreLoad : Page
+    {
+        public static bool IfDone = false;
+        public static string FileSavePath;
+        public static string IPAdressOfServer;
+        public PreLoad()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FileSavePath = "Error";
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+            {
+                FileSavePath = dialog.FileName;
+
+            }
+            PathButton1.Content = FileSavePath;
+            if (FileSavePath != "Error")
+            {
+                TextNenych.Visibility = Visibility.Hidden;
+                ReadyToGo.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            IfDone = true;
+            IPAdressOfServer = IPAdressOfServer1.Text;
+            if (PreLoad.IfDone == true)
+            {
+               MainWindow.DownloadGamesList();
+            }
+            if (MainWindow.temp != 0)
+            {
+                NavigationService.Navigate(new Games());
+            }
+        }
+    }
+}
